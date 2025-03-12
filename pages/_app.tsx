320e0e7f1276fa48e37a855e7a8229c439915771
nutboltz <eunicehx920@gmail.com>
2025-03-12 18:16:33 -0400

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import RootLayout from "@/components/layout/RootLayout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
